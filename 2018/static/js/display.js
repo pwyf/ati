pwyf/ati

commit a0792338bc474f2b57a7aa1ceb8c413d326e20a9

Use data to generate filter groupings javascript

2018/static/js/display.js
@@ -1,10 +1,12 @@
+---
+---
+
+{% raw %}
 $(document).ready(function() {
   // Comparison Agencies
-  var usAgencies = ['us-usaid', 'us-mcc', 'us-state', 'us-pepfar', 'us-defense'];
-  var euAgencies = ['belgium-dgcd', 'denmark-mfa', 'eib', 'ec-devco', 'ec-echo', 'ec-near', 'finland-mfa', 'france-afd', 'france-meae', 'ireland-irishaid', 'italy-mae', 'netherlands-mfa', 'spain-maec-aecid', 'sweden-mfa-sida', 'uk-dfid', 'uk-fco'];
-  var dfiAgencies = ['afdb', 'asdb', 'ebrd', 'eib', 'iadb', 'world-bank-ida', 'world-bank-ifc'];
-  var unAgencies = ['unicef', 'undp', 'un-ocha'];
-  var bilateralAgencies = ['uk-dfid', 'uk-fco', 'sweden-mfa-sida', 'canada-dfatd', 'netherlands-mfa','us-usaid', 'us-mcc', 'us-state', 'us-pepfar', 'us-defense', 'germany-bmz-giz', 'germany-bmz-kfw', 'switzerland-sdc', 'australia-dfat', 'denmark-mfa','france-afd', 'france-meae', 'italy-mae', 'finland-mfa', 'belgium-dgcd', 'japan-jica', 'ireland-irishaid', 'spain-maec-aecid', 'norway-mfa', 'korea-koica', 'new-zealand-mfat', 'uae-ministry-of-foreign-affairs', 'china-mofcom'];
+  {% endraw %}
+  var comparisonAgencies = {{ site.data['2018'].filtergroupings | jsonify }};
+  {% raw %}
 
   $('select').SumoSelect();
 
@@ -13,42 +15,27 @@ $(document).ready(function() {
     $('#compareSel').on('change', function(event) {
       $('#done').hide();
       $('.results-table').removeClass('compare-active');
+
       var compare = $('#compareSel').val();
-      switch (compare) {
-        case 'custom' :
-          $('.result-row').addClass('compare-shown').fadeIn();
-          $('.results-table').addClass('compare-active');
-          $('#done').show().css('display', 'inline-block');
-          $('#done').click(function() {
-            $(this).text('update');
-            $('.result-row').removeClass('compare-shown');
-            var checked = $('.checkbox:checkbox:checked');
-            $.each(checked, function(i, val) {
-              var thisRow = val.closest('.result-row');
-              $(thisRow).addClass('compare-shown');
-            })
-            $('.result-row').not('.compare-shown').fadeOut();
-            $(this).hide();
-            })
-          break;
-        case 'US' :
-          compareAgencies(usAgencies);
-          break;
-        case 'EU' :
-          compareAgencies(euAgencies);
-          break;
-        case 'UN' :
-          compareAgencies(unAgencies);
-          break;
-        case 'DFI' :
-          compareAgencies(dfiAgencies);
-          break;
-        case 'BILATERAL' :
-          compareAgencies(bilateralAgencies);
-          break;
-        case '' :
-          resetSelect();
-          break;
+      if ($.inArray(compare, comparisonAgencies)) {
+        compareAgencies(comparisonAgencies[compare]);
+      } else if (compare === 'custom') {
+        $('.result-row').addClass('compare-shown').fadeIn();
+        $('.results-table').addClass('compare-active');
+        $('#done').show().css('display', 'inline-block');
+        $('#done').click(function() {
+          $(this).text('update');
+          $('.result-row').removeClass('compare-shown');
+          var checked = $('.checkbox:checkbox:checked');
+          $.each(checked, function(i, val) {
+            var thisRow = val.closest('.result-row');
+            $(thisRow).addClass('compare-shown');
+          })
+          $('.result-row').not('.compare-shown').fadeOut();
+          $(this).hide();
+          })
+      } else if (compare === '') {
+        resetSelect();
       }
       $('#cancel').text('reset');
     });
@@ -222,3 +209,4 @@ function rangeSet() {
     }
   });
 }
+{% endraw %}

_layouts/2018/homepage.html
@@ -73,25 +73,11 @@ layout: 2018/base
 										Select comparison
 									</option>
 
-									<option value="BILATERAL">
-										Bilateral Agencies
+									{% for filtergrouping in site.data['2018'].filtergroupings %}
+									<option value="{{ filtergrouping[0] }}">
+										{{ filtergrouping[0] }}
 									</option>
-
-									<option value="DFI">
-										Development Finance
-									</option>
-
-									<option value="EU">
-										European Union
-									</option>
-
-									<option value="UN">
-										United Nations
-									</option>
-
-									<option value="US">
-										United States
-									</option>
+									{% endfor %}
 
 									<option value="custom">
 										Custom
@@ -102,14 +88,10 @@ layout: 2018/base
 									<ul class="options">
 										<li class="opt selected"><label>Select comparison</label>
 										</li>
-										<li class="opt"><label>US Agencies</label>
+										{% for filtergrouping in site.data['2018'].filtergroupings %}
+										<li class="opt"><label>{{ filtergrouping[0] }}</label>
 										</li>
-										<li class="opt"><label>UN Agencies</label>
-										</li>
-										<li class="opt"><label>EU Agencies</label>
-										</li>
-										<li class="opt"><label>DFIs</label>
-										</li>
+										{% endfor %}
 										<li class="opt"><label>Custom</label>
 										</li>
 									</ul>

commit 7bd5f0c2b6c28bae2b38a4ca9374df6991bb440f

Fix bug introduced in a0792338

2018/static/js/display.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
       $('.results-table').removeClass('compare-active');
 
       var compare = $('#compareSel').val();
-      if ($.inArray(compare, comparisonAgencies)) {
+      if (compare in comparisonAgencies) {
         compareAgencies(comparisonAgencies[compare]);
       } else if (compare === 'custom') {
         $('.result-row').addClass('compare-shown').fadeIn();